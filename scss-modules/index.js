78cfb2a6a7c7a908799347d88045deade2bcd0b9
nanxiaobei <nanxiaobei@gmail.com>
2021-09-14 05:35:28 +0800

    refine style

--- scss-modules/index.js
+++ scss-modules/index.js
@@ -2,11 +2,6 @@ module.exports = {
   extends: ['../scss', 'stylelint-config-css-modules'],
   rules: {
     'at-rule-no-unknown': null,
-    'scss/at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['value'],
-      },
-    ],
+    'scss/at-rule-no-unknown': [true, { ignoreAtRules: ['value'] }],
   },
 };
